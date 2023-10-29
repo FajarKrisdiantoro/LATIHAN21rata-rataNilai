7be47a96b8ecf1be618a53f480a80863c5d62b3b
LENOVO <LENOVO@DESKTOP-VL4046V>
2023-10-29 20:45:24 +0700

    Project PBO PROGRAM RATA RATA NILAI

---
+++ src/projectpbogit21/ProjectPBOgit21.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectpbogit21;
+import java.util.Scanner;
+/**
+ *
+ * @author LENOVO
+ */
+public class ProjectPBOgit21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            
+        System.out.print("Masukkan banyaknya mahasiswa: ");
+        int banyakMahasiswa = input.nextInt();
+
+        int totalNilai = 0;
+        for (int i = 1; i <= banyakMahasiswa; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            int nilaiMahasiswa = input.nextInt();
+            totalNilai += nilaiMahasiswa;
+        }
+
+        double rataRata = (double) totalNilai / banyakMahasiswa;
+
+        System.out.println("Banyaknya mahasiswa: " + banyakMahasiswa);
+        System.out.println("Total nilai mahasiswa: " + totalNilai);
+        System.out.println("Rata-rata nilai mahasiswa: " + rataRata);
+
+        input.close();
+    }
+    
+}
